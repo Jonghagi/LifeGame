0df6797ee1befa8c2b525a26cc5be81b5c4a1325
Jonghagi <jonghagi77@gmail.com>
2016-05-02 13:09:37 +0900

    initallize project and make cell class

---
+++ src/Cell.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Kimjonghak on 2016. 5. 2..
+ */
+public class Cell {
+    private boolean status;
+
+    public void Die(){
+        status = false;
+    }
+
+    public void Live(){
+        status = true;
+    }
+
+    public boolean currentStatus(){
+        return status;
+    }
+}
---
+++ src/LifeGame.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Kimjonghak on 2016. 5. 2..
+ */
+public class LifeGame {
+    public static void main(){
+        Cell[][] cell;
+
+        cell = new Cell[10][10];
+
+        
+    }
+}
