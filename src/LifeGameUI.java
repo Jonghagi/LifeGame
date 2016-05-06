7a296cf1cbcf04fd89533c693bd0d53f159a4f65
Jonghagi <jonghagi77@gmail.com>
2016-05-06 16:13:12 +0900

    add Input text(board Size) and Stop button

--- src/LifeGameUI.java
+++ src/LifeGameUI.java
@@ -8,16 +8,66 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.TimerTask;
 import java.util.Timer;
+
 public class LifeGameUI extends JFrame {
-    JPanel[][] CellUI = new JPanel[60][60];
+    JPanel[][] CellUI;
     JButton StartButton = new JButton("Start");
+    JButton SettingButton = new JButton("Setting");
+    JButton StopButton = new JButton("Stop");
+    JTextField inputN = new JTextField();
     LifeGameEngine Engine;
-    int N = 60;
+    Timer timer;
     boolean isGaming;
+    int N;
     Cell[][] cells;
     public void start(){
-        cells = new Cell[N][N];
+        isGaming = false;
+        inputN.setBounds(10, 10, 50, 30);
+        SettingButton.setBounds(60, 10, 100, 30);
+        add(inputN);
+        add(SettingButton);
         Engine = new  LifeGameEngine();
+        add(StartButton);
+        setLayout(null);
+        setVisible(true);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(500, 100);
+        StartButton.setBounds(170, 10, 100, 30);
+        StopButton.setBounds(270, 10, 100, 30);
+        add(StopButton);
+
+        StartButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(!isGaming) {
+                    isGaming = true;
+                    GameStart();
+                }
+            }
+        });
+
+        StopButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(isGaming){
+                    timer.cancel();
+                    isGaming = false;
+                }
+            }
+        });
+        SettingButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                N = Integer.parseInt(inputN.getText());
+                SettingMap();
+            }
+        });
+
+    }
+
+    private void SettingMap(){
+        cells = new Cell[N][N];
+        CellUI = new JPanel[N][N];
 
         for(int i = 0; i < N; i++){
             for(int j = 0; j < N; j++){
@@ -26,9 +76,8 @@ public class LifeGameUI extends JFrame {
         }
 
 
-        isGaming = false;
-        for(int i = 0; i < 60; i++)
-            for (int j = 0; j < 60; j++) {
+        for(int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++) {
                 CellUI[i][j] = new JPanel();
                 CellUI[i][j].setBounds(i * 10, (j * 10) + 80, 10, 10);
                 if(cells[i][j].currentStatus()){
@@ -39,18 +88,7 @@ public class LifeGameUI extends JFrame {
                 add(CellUI[i][j]);
             }
 
-        StartButton.setBounds(100, 10, 100, 50);
-        StartButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                GameStart();
-            }
-        });
-        add(StartButton);
-        setLayout(null);
-        setVisible(true);
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setSize(600, 600);
+        setSize(N * 10, N * 10 + 80);
 
         addMouseListener(new MouseListener() {
             @Override
@@ -87,21 +125,17 @@ public class LifeGameUI extends JFrame {
             public void mouseExited(MouseEvent e) {}
         });
     }
+    private void Die(int x, int y){CellUI[x][y].setBackground(Color.white);}
 
-    public void Die(int x, int y){
-        CellUI[x][y].setBackground(Color.white);
-    }
+    private void Live(int x, int y){CellUI[x][y].setBackground(Color.black);}
 
-    public void Live(int x, int y){ CellUI[x][y].setBackground(Color.black);
-    }
-
-    public void GameStart(){
+    private void GameStart(){
         System.out.println("Start");
-        Timer timer = new Timer();
+        timer = new Timer();
         timer.schedule(new EngineRun(), 100, 100);
     }
 
-    public class EngineRun extends TimerTask {
+    private class EngineRun extends TimerTask {
         @Override
         public void run() {
             Engine.checkCells(cells, N);
